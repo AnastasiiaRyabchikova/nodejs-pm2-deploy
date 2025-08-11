037be7229a6df36cea3df5f57f86cc5e065aa3b9
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2025-08-11 22:41:04 +0300

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: 'https://github.com/AnastasiiaRyabchikova/nodejs-pm2-deploy',
       path: DEPLOY_PATH,
       // 'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH} `,
-      'pre-deploy': 'ifconfig',
+      'pre-deploy': 'ip',
       'post-deploy': 'npm i && npm run build',
       ssh_options: [
         'StrictHostKeyChecking=no',
