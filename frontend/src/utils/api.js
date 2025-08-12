fa2d42fa2bb58d0f78f22af08a5aa4eafdc713b1
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2025-08-12 16:38:23 +0300

    fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.ryabchulya.nomorepartiessbs.ru');
+  const api = new Api('https://api.project.ryabchulya.nomorepartiessbs.ru');
 
   export default api;
