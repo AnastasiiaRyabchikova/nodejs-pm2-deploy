f2e515f61cc59a50a88aa61b5f3d2157dbd3418e
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2025-08-12 11:32:05 +0300

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: 'https://github.com/AnastasiiaRyabchikova/nodejs-pm2-deploy',
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash ./scripts/deployEnv.sh ${process.env.DEPLOY_USER}@${process.env.DEPLOY_HOST} ${process.env.DEPLOY_PATH}`,
-      'post-deploy': 'cd backend npm i && npm run build',
+      'post-deploy': 'cd backend && npm ci && npm run build pm2 startOnRestart ecosystem.config.js --env production',
     },
   },
 };
