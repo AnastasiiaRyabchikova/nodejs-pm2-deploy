2e9622bc52e44fcba6a8944220be8d99fe2b9488
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2025-08-12 18:36:25 +0300

    LAST

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/AnastasiiaRyabchikova/nodejs-pm2-deploy',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'post-deploy': 'cd frontend && npm ci && npm run build',
     },
