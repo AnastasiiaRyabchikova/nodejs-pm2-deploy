67f1f341a3836e0a0e5ac1b79afe065ac9c2971f
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2025-08-12 17:50:36 +0300

    fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,10 +14,7 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors({
-//   origin: 'https://project.ryabchulya.nomorepartiessbs.ru',
-//   credentials: true,
-// }));
+// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
